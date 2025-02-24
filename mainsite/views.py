e87113705da165e3e13505bd4f5bd89058e38143
RobinMguif <robin.in@mguif.com>
2025-02-24 14:53:21 +0530

    adding from robin on 2nd time 24-02-2025

--- mainsite/views.py
+++ mainsite/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render, HttpResponse
 def home_view(request):
     # return HttpResponse('Hello World')
     return render(request, 'mainsite/home.html',)
+
+
+def login(request):
+    return render(request,'mainsite/login.html')
+
+
+def registration(request):
+    return render(request,'mainsite/registration.html')
