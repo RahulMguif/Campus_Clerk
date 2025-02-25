d18f2a47e1293b1e5b5f54db88f1da19a0273e21
Reny-Joseph <reny.in@mguif.com>
2025-02-25 17:16:24 +0530

    added staff advisor page

--- office_admin/models.py
+++ office_admin/models.py
@@ -9,7 +9,7 @@ class course(models.Model):
 
 
 class departments(models.Model):
-    couse_pk=models.ForeignKey(course, on_delete=models.CASCADE)
+    course_pk=models.ForeignKey(course, on_delete=models.CASCADE)
     department_name = models.CharField(max_length = 200, null=True)
     delete_status = models.BooleanField(default=False)
    
--- office_admin/urls.py
+++ office_admin/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('logout/',views.logout_view,name='logout'),
     path('course_config/',views.course_config,name='course_config'),
     path('department_config',views.department_config,name='department_config'),
+    path('add_staff_advisor',views.add_staff_advisor,name='add_staff_advisor'),
+    path('edit_staff_advisor',views.edit_staff_advisor,name='edit_staff_advisor'),
+    path('edit_staff_advisor',views.edit_staff_advisor,name='edit_staff_advisor'),
+    path('delete_staff_advisor',views.delete_staff_advisor,name='delete_staff_advisor'),
+    path('change_admin_status/<int:admin_pk>/',views.change_admin_status,name='change_admin_status'),
+
+
 ]
