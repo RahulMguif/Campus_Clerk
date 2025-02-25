f20f59b72ed4d1ee62c6c4444c39625611d14b09
RobinMguif <robin.in@mguif.com>
2025-02-25 12:45:54 +0530

    adding from robin on 3rd time on 25-02-2025

--- office_admin/urls.py
+++ office_admin/urls.py
@@ -4,5 +4,7 @@ from office_admin import views
 urlpatterns = [
     path('admin_home/',views.admin_home,name='admin_home'),
     path('office_admin_login/',views.office_admin_login,name='office_admin_login'),
-    path('logout/',views.logout_view,name='logout')
+    path('logout/',views.logout_view,name='logout'),
+    path('course_config/',views.course_config,name='course_config'),
+    path('department_config',views.department_config,name='department_config'),
 ]
