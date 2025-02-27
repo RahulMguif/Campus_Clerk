bcb396b4d86b92c332f807b1942ebc233779cc9f
RahulMguif <rahulmguif@gmail.com>
2025-02-27 17:31:08 +0530

    staff advisor update application form working

--- staff_advisor/urls.py
+++ staff_advisor/urls.py
@@ -3,5 +3,6 @@ from staff_advisor import views
 
 urlpatterns = [
     path('staff_home/',views.staff_home,name='staff_home'),
-
+    path('view_student_applications',views.view_student_applications,name='view_student_applications'),
+    path('staff_advisor_review_application/<int:application_id>/', views.staff_advisor_review_application, name='staff_advisor_review_application'),
 ]
--- staff_advisor/views.py
+++ staff_advisor/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from staff_advisor.models import *
 from django.contrib import messages
+from student.models import *
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def staff_home(request):
     return render(request,"staff_advisor/home.html")
 
+def view_student_applications(request):
+    application_details = student_application_request.objects.all()
+    return render(request,'staff_advisor/view_student_applications.html',{'application_details':application_details})    
+
+
+
+def staff_advisor_review_application(request, application_id):
+    application_entry = get_object_or_404(student_application_request, id=application_id)
+
+    if request.method == 'POST':
+        office_remark = request.POST.get('office_remark', '')
+        office_signature = request.FILES.get('office_signature')
+
+        # Update office remark
+        application_entry.office_remark = office_remark
+
+        # Handle office signature file upload
+        if office_signature:
+            fs = FileSystemStorage(location='media/office_signatures/')
+            filename = fs.save(office_signature.name, office_signature)
+            application_entry.office_signature_url = f'media/office_signatures/{filename}'
+
+        # Save changes
+        application_entry.save()
+        print("saved application")
+        messages.success(request, "Application updated successfully!")
+        return redirect('view_applications')  # Adjust based on your URL name
+
+    return render(request, 'office_admin/update_application.html', {'application_entry': application_entry})
